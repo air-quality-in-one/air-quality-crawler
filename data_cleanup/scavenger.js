75536b41e07286ce4a1e5f366bab1375dcd8f49c
Xu,Lvcai <lvxu@paypal.com>
2015-06-09 14:34:18 +0800

    test

--- data_cleanup/scavenger.js
+++ data_cleanup/scavenger.js
@@ -9,7 +9,7 @@ var AQIHistory = require('../models/aqi_history');
 var OverdueAirQuality = require('../models/overdue_air_quality');
 
 function Scavenger() {
-	this.job = new CronJob('00 20 14 * * *', 
+	this.job = new CronJob('00 37 14 * * *', 
 		cleanup, null, false, 'Asia/Shanghai');
 }
 
@@ -42,42 +42,14 @@ function cleanup () {
 			//cursor(0, 10, function(overdue_air_quality) {
 				//console.log("Success to remove : " + overdue_air_quality);
 			//});
+  removeOverdueAirQuality();
 		}
 	});
 }
 
-
-function cursor (_start,_limit,_callback){
-  //初始化数据定义
-  var start,limit,flag,len;
-  //初始化起始位置
-  start = !_start || _start < 0 ? 0 : _start;
-  //初始化分页数量
-  limit = !_limit || _limit < 1 ? 1 : _limit;
-
-  console.log("From " + _start + " to " + (_start + _limit));
-  //使用Model执行分页查询
-  OverdueAirQuality.find().skip(start).limit(limit).exec(function(err,docs){
-    //缓存长度
-    len = docs.length;
-    //如果没有查询到，证明已经查询完毕
-    if(len === 0){
-      console.log('遍历结束');
-    }
-    //初始化循环结束标记
-    flag = 0;
-    //遍历
-    docs.forEach(function(doc){
-      //如果有执行函数就执行
-      if(_callback && toString.call(_callback) === '[object Function]'){
-        _callback(doc);
-      }
-      //如果循环到末尾，则迭代
-      if(len == ++flag){
-      	//console.log("Try to call recursively method!");
-        cursor(start + docs.length,limit, _callback);
-      }
-    });
+function removeOverdueAirQuality() {
+  OverdueAirQuality.find().exec(function(err,docs){
+    console.log("find " + docs.length + " overdue air quality!");
   });
 }
 
